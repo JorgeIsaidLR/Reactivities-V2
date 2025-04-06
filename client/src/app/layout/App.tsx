8d1f53036a8455573f84927c42b8ca6ad4b9d677
JorgeIsaidLR <lopezjorgeisaid@gmail.com>
2025-04-06 00:27:32 -0600

    End of secction 8

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,18 +1,25 @@
 import { Box, Container, CssBaseline} from "@mui/material";
 import NavBar from "./NavBar";
 import { Outlet } from "react-router";
+import HomePage from "../../features/home/HomePage";
+import { useLocation } from "react-router";
 
 
 function App() {
+  const location = useLocation();
   
-
   return (
     <Box sx={{ bgcolor: '#eeeeee', minHeight:'100vh' }}>
       <CssBaseline />
+      {location.pathname === '/' ? <HomePage/> :(
+        <>
       <NavBar />
       <Container maxWidth='xl' sx={{ mt: 3 }}>
       <Outlet/>
-      </Container> 
+      </Container>   
+        </>
+      )}
+      
     </Box>
   )
 }
